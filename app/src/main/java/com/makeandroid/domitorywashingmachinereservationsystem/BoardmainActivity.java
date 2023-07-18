59ef0fcee46aa06f3a9dad436e724fa93e88d4f7
Kyongmin Yea <kminnyes.gmail.com>
2023-07-18 12:58:42 +0900

    게시판 디자인 구현

--- app/src/main/java/com/makeandroid/domitorywashingmachinereservationsystem/BoardmainActivity.java
+++ app/src/main/java/com/makeandroid/domitorywashingmachinereservationsystem/BoardmainActivity.java
@@ -1,10 +1,23 @@
 package com.makeandroid.domitorywashingmachinereservationsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
 
 public class BoardmainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mFirebaseAuth; // 파이어베이스 인증
+    private DatabaseReference mDatabaseRef; //실시간 데이터 베이스
+
+    private RecyclerView mPostRecyclerView;
+    private ImageButton backtoboardmain;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/makeandroid/domitorywashingmachinereservationsystem/BoardwriteActivity.java
@@ -0,0 +1,14 @@
+package com.makeandroid.domitorywashingmachinereservationsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BoardwriteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_boardwrite);
+    }
+}
